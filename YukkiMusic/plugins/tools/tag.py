f6eb76a6f9213ac4c7ac1c55499746724cc4cddb
Ahmad Qassem <121644358+DevKassem@users.noreply.github.com>
2023-02-10 03:59:22 +0300

    Update tag.py

--- YukkiMusic/plugins/tools/tag.py
+++ YukkiMusic/plugins/tools/tag.py
@@ -2,7 +2,6 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 import os
 import asyncio
-from pyrogram import enums
 from pyrogram import ChatMemberStatus
 from pyrogram.errors import FloodWait
 from YukkiMusic import app
