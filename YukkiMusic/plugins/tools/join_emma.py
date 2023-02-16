a8b3ae21feee9e9a90091998722bece98d5c1b17
EmmaMusicBot <125144392+EmmaMusicBot@users.noreply.github.com>
2023-02-17 01:11:38 +0300

    Update join_emma.py

--- YukkiMusic/plugins/tools/join_emma.py
+++ YukkiMusic/plugins/tools/join_emma.py
@@ -6,27 +6,27 @@ from YukkiMusic import app
 
 @app.on_message(~filters.edited & filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
-    if not "https://t.me/WoPictures":  # Not compulsory
+    if not "https://t.me/D_33_D":  # Not compulsory
         return
     try:
         try:
-            await bot.get_chat_member("WoPictures", msg.from_user.id)
+            await bot.get_chat_member("D_33_D", msg.from_user.id)
         except UserNotParticipant:
-            if "https://t.me/WoPictures".isalpha():
-                link = "https://t.me/WoPictures"
+            if "https://t.me/D_33_D".isalpha():
+                link = "https://t.me/D_33_D"
             else:
-                chat_info = await bot.get_chat("WoPictures")
+                chat_info = await bot.get_chat("D_33_D")
                 link = chat_info.invite_link
             try:
                 await msg.reply(
                     f"⌯︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton(u"World Pictures", url=link)]
+                        [InlineKeyboardButton(u"🦋 ʚɞ 🦋", url=link)]
                     ])
                 )
                 await msg.stop_propagation()
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"عليك رفع البوت آدمن في القناة أولاً ؟؟ : @WoPictures !")
+        print(f"عليك رفع البوت آدمن في القناة أولاً ؟؟ : @D_33_D !")
