bf8711db54e98799150187f979510ff4e4d40dca
EmmaMusicBot <125144392+EmmaMusicBot@users.noreply.github.com>
2023-02-18 02:10:28 +0300

    Update join_emma.py

--- YukkiMusic/plugins/tools/join_emma.py
+++ YukkiMusic/plugins/tools/join_emma.py
@@ -6,16 +6,16 @@ from YukkiMusic import app
 
 @app.on_message(~filters.edited & filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
-    if not "https://t.me/D_33_D":  # Not compulsory
+    if not "https://t.me/Butter_flyf":  # Not compulsory
         return
     try:
         try:
-            await bot.get_chat_member("D_33_D", msg.from_user.id)
+            await bot.get_chat_member("Butter_flyf", msg.from_user.id)
         except UserNotParticipant:
-            if "https://t.me/D_33_D".isalpha():
-                link = "https://t.me/D_33_D"
+            if "https://t.me/Butter_flyf".isalpha():
+                link = "https://t.me/Butter_flyf"
             else:
-                chat_info = await bot.get_chat("D_33_D")
+                chat_info = await bot.get_chat("Butter_flyf")
                 link = chat_info.invite_link
             try:
                 await msg.reply(
@@ -29,4 +29,4 @@ async def must_join_channel(bot: Client, msg: Message):
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"عليك رفع البوت آدمن في القناة أولاً ؟؟ : @D_33_D !")
+        print(f"عليك رفع البوت آدمن في القناة أولاً ؟؟ : @Butter_flyf !")
